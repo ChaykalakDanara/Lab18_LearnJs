ad499a6a155cf48de1af8f8390b7dd9c37c5cbb1
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-04-09 22:14:37 +0300

    Step 11: Node.js installed and main.js executed

--- main.js
+++ main.js
@@ -16,24 +16,31 @@ value = true;
 console.log(value);
 
 let userName = "Данара";
-
-console.log('Привет, ${userName}!')
-
+console.log(`Привет, ${userName}!`)
 let price = 99.99;
 let temperature = -15;
 let infinity = 1/0;
 let notANumber = 0/0;
 console.log(0.1 + 0.2);
 
-let bigNumber = 
+let bigNumber = 9007199254740991n;
+let huge = BigInt("123456789012345678901234567890");
+
+let isAlive = true;
+let isWorking = false;
+let isAdult = age >= 18;
+
+let x;
+let y = undefined;
 
 let person = {
-    name: "Nataliya",
-    age: 19,
-    isStudent: function () {
+    name: "Станислав",
+    age: 30,
+    isStudent: false,
+    sayHello: function () {
         console.log("Привет!");
     }
-}
+};
 console.log(person.name);
 
 let fruits = ["яблоко","банан","апельсин"];
@@ -58,7 +65,70 @@ console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
+
 console.log(10 + "5");
 console.log("10" - 5);
 
-co
+
+const numbersArray = [1, 2, 3];
+numbersArray[0] = 10;
+console.log(numbersArray);
+
+// const persons = { name: "Denis", age: 18 };
+// persons.age = 50;
+// persons.city = "Volgograd";
+// console.log(persons);
+
+// person = {name: "Stas"}
+
+console.log(typeof "текст");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {});
+
+let numberX = null;
+console.log(numberX === null);
+
+let newPrice = 100;
+console.log(newPrice, typeof newPrice);
+
+newPrice = "сто рублей";
+console.log(newPrice, typeof newPrice);
+
+let num = 42;
+let str = String(num); 
+let str2 = num.toString();
+let str3 = "" + num;
+
+let strNum = "123";
+let int = Number(strNum); 
+let int2 = parseInt("42.5"); 
+let float = parseFloat("3.14"); 
+let int3 = +"99"; 
+
+let bool1 = Boolean(1); 
+let bool2 = !!1;
+let bool3 = Boolean(0); 
+let bool4 = Boolean(""); 
+console.log(Number("123"));
+console.log(String(123));
+console.log(Boolean(1));
+
+console.log("5" + 3);
+console.log("5" - 3);
+console.log("5" * "2");
+console.log(true + 1);
+console.log(false + 1);
+console.log(null + 1);
+console.log(undefined + 1);
+
+// 11.4 
+let a2 = 8;
+let b2 = 4;
+
+console.log("Сложение (a2 + b2):", a2 + b2);
+console.log("Умножение (a2 * b2):", a2 * b2);
