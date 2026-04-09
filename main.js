f54a1cee4d41a01a030df3d76f542f915a40714d
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-04-09 16:54:35 +0300

    Step 6: variables and data types in JavaScript

---
+++ main.js
@@ -0,0 +1,64 @@
+console.log("Hello, JavaScript!")
+
+let age = 20;
+let name = "Danara";
+let isStudent = true;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Is student: ", isStudent);
+
+let value = 10;
+console.log(value);
+value = "Терперь это строка";
+console.log(value);
+value = true;
+console.log(value);
+
+let userName = "Данара";
+
+console.log('Привет, ${userName}!')
+
+let price = 99.99;
+let temperature = -15;
+let infinity = 1/0;
+let notANumber = 0/0;
+console.log(0.1 + 0.2);
+
+let bigNumber = 
+
+let person = {
+    name: "Nataliya",
+    age: 19,
+    isStudent: function () {
+        console.log("Привет!");
+    }
+}
+console.log(person.name);
+
+let fruits = ["яблоко","банан","апельсин"];
+let numbers = [1, 2, 3, 4, 5];
+let mixed = ["текст", 42, true, null];
+
+function sum(a, b){
+    return a + b;
+}
+let multiply = function (x, y) {
+    return x * y;
+}
+console.log(sum(5,3));
+
+let now = new Date();
+let birthday = new Date("1995-12-17");
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(10 + "5");
+console.log("10" - 5);
+
+co
